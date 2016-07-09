5554c45b057fe7c25b63163fee9d52f2f132b16d
Truong Hoang Dung <checkraiser11@gmail.com>
2016-07-09 17:07:19 +0700

    Init

---
+++ lib/as_json_with_includes.rb
@@ -0,0 +1,38 @@
+require "as_json_with_includes/version"
+
+module ActiveRecord
+  class Base
+    def as_json(options = {})
+      super(options).merge(serializable_hash(options))
+    end
+  end
+
+  module Calculations
+    def convert_activerecord_includes_to_json_includes(active_record_includes)
+      return active_record_includes if active_record_includes.is_a? Symbol
+      temp_arr = []
+      temp_hash = {}
+      if active_record_includes.is_a? Array
+        active_record_includes.each do |item|
+          temp_arr << convert_activerecord_includes_to_json_includes(item)
+        end
+        return temp_arr
+      end
+      if active_record_includes.is_a? Hash
+        active_record_includes.each do |key, value|
+          temp_hash[key] = {include: convert_activerecord_includes_to_json_includes(value)}
+        end
+        temp_hash
+      end
+    end
+
+    def as_json_with_includes(options)
+      active_record_includes = options[:includes]
+      return as_json(options) if active_record_includes.nil?
+      json_inclusions = convert_activerecord_includes_to_json_includes(active_record_includes)
+      options[:include] = json_inclusions
+      options[:includes] = nil
+      includes(active_record_includes).as_json(options)
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'as_json_with_includes'
