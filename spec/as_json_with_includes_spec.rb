0697beca3a7cfe117e4a1ab670717ec6242b9550
Truong Hoang Dung <checkraiser11@gmail.com>
2016-07-09 19:30:54 +0700

    Fix travis

--- spec/as_json_with_includes_spec.rb
+++ spec/as_json_with_includes_spec.rb
@@ -1,18 +1,22 @@
 require 'spec_helper'
 
 describe AsJsonWithIncludes do
-  class Dummy
-    extend AsJsonWithIncludes::CalculationsPatch
-  end
   it 'has a version number' do
     expect(AsJsonWithIncludes::VERSION).not_to be nil
   end
 
   describe 'AsJsonWithIncludes::CalculationsPatch' do
+    class Dummy
+      extend AsJsonWithIncludes::CalculationsPatch
+    end
     it 'convert active record includes to as_json includes' do
       input = [:a, :b, :c, :d, {:e => {:f => [:g, :h]}}]
       expected = [:a, :b, :c, :d, {:e => {:include => {:f => {:include => [:g, :h]}}}}]
       expect(Dummy.convert_activerecord_includes_to_json_includes(input)).to eql(expected)
     end
+  end
+
+  describe 'AsJsonWithIncludes::BasePatch' do
+    # TODO
   end
 end
